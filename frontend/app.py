cf7c002eae5f5fd69b1deab88ff9cf2e2ee14767
caelen <x2021bix@stfx.ca>
2026-06-20 20:58:43 -0300

    Fix formatting of comments in app.py

--- frontend/app.py
+++ frontend/app.py
@@ -700,7 +700,7 @@ class Window(Adw.ApplicationWindow):
             developer_name="VaultSend",
             version="0.1.0",
             comments="File and text encryption using the age format. "
-            "All cryptography is performed by a small separate backend.",
+            "All cryptography is performed by a small separate backend."
             "Source code can be found here: https://github.com/caelenm/vaultsend/",
         )
         about.present(self)
